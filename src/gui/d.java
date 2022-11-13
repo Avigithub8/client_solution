dcdd66ed403972dd11bb856bf9cb44fef3e5d947
Avi <sharma.avi844@gmail.com>
2022-11-13 23:16:24 +0530

    Initial Commit

---
+++ src/gui/d.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Avnish Sharma
+ */
+class d {
+
+    static Date getDate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void setDate(Date dob) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void setSelectableDateRange(Date date, Date date0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
